ce3712795e807f354f69a1affd4246ae67b457b3
John Oram <john@oram.ca>
2023-07-13 15:07:51 -0700

    and surenames

--- generate_data.py
+++ generate_data.py
@@ -2,7 +2,7 @@
 
 male_data = {}
 female_data = {}
-with open("./17100147.csv", "r") as f:
+with open("./firstnames.csv", "r") as f:
     lines = f.readlines()
     for line in lines:
         parts = line.split(",")
@@ -20,6 +20,20 @@ with open("./17100147.csv", "r") as f:
                     female_data[name] = 0
                 female_data[name] += float(value)
 
+surnames = {}
+# name,rank,count,prop100k,cum_prop100k,pctwhite,pctblack,pctapi,pctaian,pct2prace,pcthispanic
+with open("./surnames.csv", "r") as f:
+    lines = f.readlines()
+    for line in lines[1:]:
+        parts = line.split(",")
+        name = parts[0].strip('"')
+        count = parts[2].strip('"')
+        if name not in surnames:
+            surnames[name] = 0
+        surnames[name] += float(count)
+
+
+
 with open("./name_generator/data.go", "w") as f:
     f.write("package name_generator\n\n")
     f.write("var MaleNames = map[string]int{\n")
@@ -32,6 +46,13 @@ with open("./name_generator/data.go", "w") as f:
     f.write("var FemaleNames = map[string]int{\n")
     min_weight = min(female_data.values())
     for name, weight in female_data.items():
+        weight = max(1, int(weight/min_weight))
+        f.write('\t"' + name + '": ' + str(weight) + ",\n")
+    f.write("}\n\n")
+
+    f.write("var Surnames = map[string]int{\n")
+    min_weight = min(surnames.values())
+    for name, weight in surnames.items():
         weight = max(1, int(weight/min_weight))
         f.write('\t"' + name + '": ' + str(weight) + ",\n")
     f.write("}\n")
--- name_generator/name_generator.go
+++ name_generator/name_generator.go
@@ -3,6 +3,7 @@ package name_generator
 type NameGenerator struct {
 	maleNames   *WeightedNameCollection
 	femaleNames *WeightedNameCollection
+	surenames   *WeightedNameCollection
 }
 
 func (ng *NameGenerator) MaleName(seed int64) string {
@@ -13,10 +14,15 @@ func (ng *NameGenerator) FemaleName(seed int64) string {
 	return ng.femaleNames.GetRandomName(seed)
 }
 
+func (ng *NameGenerator) Surename(seed int64) string {
+	return ng.surenames.GetRandomName(seed)
+}
+
 func NewNameGenerator() *NameGenerator {
 	ng := &NameGenerator{
 		maleNames:   NewWeightedNameCollection(&MaleNames),
 		femaleNames: NewWeightedNameCollection(&FemaleNames),
+		surenames:   NewWeightedNameCollection(&Surnames),
 	}
 	return ng
 }
